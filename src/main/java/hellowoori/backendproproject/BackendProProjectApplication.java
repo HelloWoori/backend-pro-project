adeea9bcefa709b7dd832a47cc2314f2372690a8
hellowoori <myahn0607@gmail.com>
2023-11-23 19:31:00 +0900

    프로젝트 생성

---
+++ src/main/java/hellowoori/backendproproject/BackendProProjectApplication.java
@@ -0,0 +1,13 @@
+package hellowoori.backendproproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendProProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendProProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hellowoori/backendproproject/BackendProProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package hellowoori.backendproproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendProProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
